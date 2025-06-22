cb6b78380aadfd00b07f3ea4a460ef53359eb9d9
ZHANG <annedaiislele@gmail.com>
2025-06-22 18:40:39 +0800

    3.0

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -21,9 +21,22 @@ def process_records(records):
 
 
 def display_results(champion_to_count, countries):
+    """Display """
+    print("Wimbledon Champions: ")
+    for name, count in champion_to_count.items():
+        print(name, count)
+    print(f"\nThese {len(countries)} countries have won Wimbledon: ")
+    print(", ".join(sorted(countries)))
 
 
 def load_records(filename):
+    records = []
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split(",")
+            records.append(parts)
+    return records
 
 
 main()
