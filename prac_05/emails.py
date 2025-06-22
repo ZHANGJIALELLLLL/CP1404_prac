75ed1d7373623cec2131842751462bdba510c1b7
ZHANG <annedaiislele@gmail.com>
2025-06-22 17:11:02 +0800

    2.0

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,6 +1,20 @@
 def main():
+    """Create dictionary"""
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = get_name_from_email(email)
+        confirmation = input(f"Is your name {name}? (Y/n) ")
+        if confirmation.upper() != "Y" and confirmation != "":
+            name = input("Name: ")
+            email_to_name[email] = name
+            email = input("Email: ")
 
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
 
+def get_name_from_email(email):
+    "Extract the name from the email"
 
 
 main()
