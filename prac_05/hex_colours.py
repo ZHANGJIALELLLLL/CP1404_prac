3f763076706f5cc39b251be45599a784cb1162e2
ZHANG <annedaiislele@gmail.com>
2025-06-22 14:42:14 +0800

    hex_colours.py.1.0

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,3 @@
+COLOUR_CODES = {"aliceblue":"#f0f8ff","amber":"#ffbf00","black":"#000000",
+                "blond":"#faf0be","camel":"#c19a6b","darkgreen":"#006400",
+                "ebony":"#555d50","eminence":"#6c3082","fawn":"#e5aa70","zomp":"#39a78e"}
