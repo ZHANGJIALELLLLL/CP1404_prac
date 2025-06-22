69b7d03cd72bbae885479d8f36bc6620f82a3b72
ZHANG <annedaiislele@gmail.com>
2025-06-22 16:48:43 +0800

    word_occurrences.py， 2.0

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,7 +1,10 @@
 # Get user input
 text = input("Text: ")
-
-
+#Splitting and statistics
+words = text.split()
+word_counts = {}
+for word in words:
+    word_counts[word] = word_counts.get(word, 0) + 1
 
 # Print sorted results with aligned columns
 for word in sorted(word_counts):
