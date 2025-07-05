de1f9ca945dc17a1ef61a115d4098919be5d4aae
ZHANG <annedaiislele@gmail.com>
2025-07-05 18:13:19 +0800

    programming_language2

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,6 +1,19 @@
 class ProgrammingLanguage:
 
-    def __init__(self):
+    def __init__(self, name,typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
 
     def __str__(self):
 
+    def is_dynamic():
+
+def run_test():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+
+    print(python)
