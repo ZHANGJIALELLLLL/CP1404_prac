a8062ca5fe02635f161b736a2838eed9c7a41c20
ZHANG <annedaiislele@gmail.com>
2025-07-05 22:22:38 +0800

    guitars

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -11,5 +11,9 @@ def main():
         print(guitar_to_add, "added.")
         name = input("Name: ")
 
+    print("\nThese are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
 
 main()
