396dafc3a65f25be60c8dab9e8ee8a6573974905
ZHANG <annedaiislele@gmail.com>
2025-07-05 22:16:04 +0800

    guitars

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,6 +1,15 @@
 from prac_06.guitar import Guitar
 def main():
+    guitars = []
     print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(guitar_to_add, "added.")
+        name = input("Name: ")
 
 
 main()
