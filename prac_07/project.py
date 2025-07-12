876961679205891f90cd59e316ff7d44b24cfa78
ZHANG <annedaiislele@gmail.com>
2025-07-12 22:30:47 +0800

    project.py

---
+++ prac_07/project.py
@@ -0,0 +1,15 @@
+class Project:
+    def __init__(self, name):
+        self.name = name
+
+
+    def __str__(self):
+        return
+
+    def __lt__(self):
+        """Project sorting"""
+        return
+
+    def is_complete(self):
+        """Determine whether the project has been completed"""
+        return
