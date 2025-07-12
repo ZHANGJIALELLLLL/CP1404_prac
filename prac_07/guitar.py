4e3dc333de5fb80a42b3b0712667faa1c3bca660
ZHANG <annedaiislele@gmail.com>
2025-07-12 21:17:28 +0800

    guitar.py2

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -1,3 +1,5 @@
+CURRENT_YEAR = 2023
+
 class Guitar:
     def __init__(self, name="", year=0, cost=0):
         self.name = name
@@ -7,6 +9,13 @@ class Guitar:
     def __str__(self):
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
 
-    def __lt__(self):
-        return
+    def __lt__(self,  other):
+        return self.year < other.year
 
+    def get_age(self):
+        """Get guitar's age."""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Determine whether it is more than 50 years."""
+        return self.get_age() >= 50
