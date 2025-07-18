1e33f39d0eeaa8579140dfacb9c376b62c6d8ce9
ZHANG <annedaiislele@gmail.com>
2025-07-19 07:27:58 +0800

    convert_miles_km.py

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -5,3 +5,12 @@ __author__ = 'Lindsay Ward'
 
 MILES_TO_KM = 1.60934
 
+class MilesConverterApp(App):
+    def build(self):
+        """ build the Kivy app from the kv file """
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_m_km_solution.kv')
+        return self.root
+
+
+MilesConverterApp().run()
