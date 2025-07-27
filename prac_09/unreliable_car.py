8044eac3bef01c2aea7da30dde92cd884463fe96
ZHANG <annedaiislele@gmail.com>
2025-07-27 16:02:09 +0800

    unreliable_car

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,2 +1,7 @@
 from random import randint
 from prac_09.car import Car
+class UnreliableCar(Car):
+    """  """
+    def __init__(self, name, fuel, reliability):
+        super().__init__(name, fuel)
+        self.reliability = reliability
