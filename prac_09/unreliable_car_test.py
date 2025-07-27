0a02815748189d5f9c0257f007d8aba68d6f28ac
ZHANG <annedaiislele@gmail.com>
2025-07-27 16:26:43 +0800

    unreliable_car test

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -1,6 +1,15 @@
 from prac_09.unreliable_car import UnreliableCar
 def main():
+    """Test UnreliableCars."""
+    good_car = UnreliableCar("Mostly Good", 100, 90)
+    bad_car = UnreliableCar("Dodgy", 100, 9)
 
+    print(f"Attempting to drive {good_car.name} and {bad_car.name} several times...")
+    for i in range(1, 12):
+        print(f"Attempt {i}:")
+        print(f"{good_car.name:12} drove {good_car.drive(i):2}km")
+        print(f"{bad_car.name:12} drove {bad_car.drive(i):2}km")
+        print("-" * 25)
 
 
 main()
