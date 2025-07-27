2e630995a41ef11d913bdbea12236aa027fdd85f
ZHANG <annedaiislele@gmail.com>
2025-07-27 16:04:35 +0800

    unreliable_car

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -5,3 +5,13 @@ class UnreliableCar(Car):
     def __init__(self, name, fuel, reliability):
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        random_chance = randint(0, 100)#random number ranging from 0 to 100
+
+        if random_chance < self.reliability:
+            distance_driven = super().drive(distance)
+        else:
+            distance_driven = 0
+
+        return distance_driven # only drive if the random number is less than the car's reliability
